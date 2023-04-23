51b5874aa6067fa39207a9b9c2900467f108f4cf
carrenjoyce <114042098+UlquiorraCifer18@users.noreply.github.com>
2023-04-23 11:06:02 +0800

    Added announcement and summary card

--- Angular-EOMSystem/src/app/components/dashboard/dashboard.component.ts
+++ Angular-EOMSystem/src/app/components/dashboard/dashboard.component.ts
@@ -53,7 +53,7 @@ export class DashboardComponent implements OnInit {
       {
         name: "Sample",
         type: "line",
-        data: [1,2,3,4,5,6,7,8,9]
+        data: [1,2,3,4,5,6,7,8,9,10,11,12]
 
       }
     ]
